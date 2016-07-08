73dab864cf19da270e87b4f3b460d7f0d9fafc81
Anri Asaturov <anri82@gmail.com>
2016-07-09 00:21:03 +0300

    Commit version in deploy message

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
+GITSHA=$(git rev-parse HEAD)
 npm run build:prod
 cd ./../peerio-web-passphrase-generator.github.io
 shopt -s extglob
 rm -rf !(CNAME|.git)
 cp -R ./../peerio-web-passphrase-generator/dist/* .
-git add -A 
-git commit -am "deploy"
+git add -A
+git commit -am "deploy $GITSHA"
 git push
 cd ./../peerio-web-passphrase-generator
